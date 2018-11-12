0da7b7249b697273d139ac49520da6c2638a4cf6
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-11-12 13:50:46 +0100

    created get req in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,56 @@ const mongoUrl = "mongodb://localhost/finalProject"
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 mongoose.Promise = Promise
 
-app.get("/", (req, res) =>
-  res.send("Hello World!")
-)
+const Park = mongoose.model("Park", {
+  title: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+  },
+  category: {
+    type: String,
+    enum: ["Hundrastgård", "Hundrastområde", "Hundbad"]
+  },
+  image: {
+    type: String,
+  },
+  rating: {
+    type: String,
+    enum: ["Dålig", "Bra", "Jättebra"]
+  }
+})
+
+// const parks = [
+//   new Park({ title: "Tantolundens Hundrastgård", location: "Tantolunden 120 57 Stockholm", description: "Stor och rymlig hundrastgård på Södermalm.", image: "http://www.basio.se/hund/126Tantoberget.jpg"  }),
+//   new Park({ title: "Monteliusvägen Hundrastgård", location: "Monteliusvägen", description: "Liten hundrastgård med utsikt fin över Stadshuset och Norrmälarstrand. Den ligger nedanför Monteliusvägen till höger om trätrappan", image: "http://www.basio.se/hund/084Montelius.jpg"  })
+// ]
+//
+// parks.forEach(parks => {
+//   parks.save().then(() => { console.log("Created", parks.title )})
+// })
+
+
+app.get("/parks", (req, res) => {
+  if (req.query.style) {
+    Park.find({ style: req.query.style }).then(parks => {
+      res.json(parks)
+    })
+  } else {
+    Park.find().then(parks => {
+      res.json(parks)
+    })
+  }
+})
 
 app.listen(8080, () => {
   console.log("Server running!")
