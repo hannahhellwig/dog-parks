fc420f41b73a0f3c4b117cc2f358f46ef8ccce54
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-11-14 16:23:29 +0100

    created form

--- frontend/src/components/add-park/add-park.js
+++ frontend/src/components/add-park/add-park.js
@@ -6,17 +6,105 @@ import Header from "components/header/header"
 
 class AddPark extends React.Component {
 
-  render() {
-    return (
-      <div className="header">
-        <Header />
-        <Link to="/">
-          <button className="addButton">Tillbaka</button>
-        </Link>
-      </div>
-
-    )
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "",
+      location: "",
+      description: "",
+      image: ""
+    }
   }
+
+    handleTitle = event => {
+      this.setState({ title: event.target.value })
+    }
+
+    handleLocation = event => {
+      this.setState({ location: event.target.value })
+    }
+
+    handleDescription = event => {
+      this.setState({ description: event.target.value })
+    }
+
+    handleImage = event => {
+      this.setState({ image: event.target.value })
+    }
+
+    handleSubmit = event => {
+      event.preventDefault()   //prevents the default behavior of submit
+
+      fetch("http://localhost:8080/parks", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.state)
+      })
+
+        .then(response => {
+          if (response.status === 201) {
+            this.setState({
+              title: "",
+              location: "",
+              description: "",
+              image: ""
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err, "ERROR")
+        })
+    }
+
+    render() {
+      return (
+        <div>
+          <div className="container">
+            <form className="addParkForm" onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder="Title"
+                name="title"
+                onChange={this.handleTitle}
+                value={this.state.title}
+                required />
+              <input
+                type="text"
+                placeholder="Plats"
+                name="Location"
+                onChange={this.handleLocation}
+                value={this.state.location}
+                required />
+              <input
+                type="text"
+                placeholder="Beskrivning"
+                name="description"
+                onChange={this.handleDescription}
+                value={this.state.description}
+                required />
+              <input
+                type="text"
+                placeholder="Bild url"
+                name="image"
+                onChange={this.handleImage}
+                value={this.state.image}
+                required />
+              <input
+                type="submit"
+                value="SEND"
+                className="button" />
+            </form>
+            <Link to="/">
+              <button className="addButton">Tillbaka</button>
+            </Link>
+          </div>
+        </div>
+
+      )
+    }
 
 }
 
