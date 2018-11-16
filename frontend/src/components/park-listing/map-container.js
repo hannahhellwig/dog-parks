115cfecec4c6d28fcf6e4bfaa3c23cc551319e78
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-11-16 11:15:03 +0100

    added markers and infowindow to google map

--- frontend/src/components/park-listing/map-container.js
+++ frontend/src/components/park-listing/map-container.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Map, GoogleApiWrapper } from "google-maps-react"
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react"
 
 const mapStyles = {
   width: "100%",
@@ -8,6 +8,29 @@ const mapStyles = {
 }
 
 export class MapContainer extends Component {
+
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  }
+
+  onMarkerClick = (props, marker) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
   render() {
     return (
       <Map
@@ -17,7 +40,23 @@ export class MapContainer extends Component {
         initialCenter={{
           lat: 59.334591,
           lng: 18.063240
-        }} />
+        }}>
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Tantolundens Hundrastgård"}
+          position={{ lat: 59.312958, lng: 18.044127 }} />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Monteliusvägen Hundrastgård"}
+          position={{ lat: 59.320731, lng: 18.060419 }} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
     )
   }
 }
